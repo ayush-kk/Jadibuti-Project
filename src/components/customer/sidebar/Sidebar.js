dc8c354d84f3f69fe5cc9b07cf70b221ad952698
ayushkumar37 <ayushkumar.official37@gmail.com>
2023-03-03 18:33:05 +0530

    Removed css conflict from update

--- src/components/customer/sidebar/Sidebar.js
+++ src/components/customer/sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Row } from "reactstrap";
 
-import "./Sidebar.css";
-import maleIcon from '../../../assets/maleIcon.jpg';
-import femaleIcon from '../../../assets/femaleIcon.jpg'
+// import "./Sidebar.css";
+import "./Sidebar.module.css";
+import maleIcon from "../../../assets/maleIcon.jpg";
+import femaleIcon from "../../../assets/femaleIcon.jpg";
 
 function Sidebar({ firstName, lastName, gender, userId }) {
   const myuser = JSON.parse(localStorage.getItem("myuser"));
@@ -12,59 +14,70 @@ function Sidebar({ firstName, lastName, gender, userId }) {
   const navigate = useNavigate();
   const doLogout = () => {
     if (myuser !== null) {
-      localStorage.removeItem("myuser")
+      localStorage.removeItem("myuser");
       alert("logged out.");
       navigate("/");
     }
-  }
-
+  };
 
   return (
-    <div className="sidebar">
-      <div className="greeting">
-
-        <img src={iconSrc} alt="icon" className="icon" />
-
-
+    <div className="sidebar-container">
+      <div className="greeting-container">
+        <Row md={7}>
+          <img src={iconSrc} alt="icon" className="icon" />
+        </Row>
         <div className="hello-text">Hello</div>
-        <div className="name-text">{firstName} {lastName}</div>
+        <div className="name-text">
+          {firstName} {lastName}
+        </div>
       </div>
-
-
-      <div className="links">
+      
+      <div className="links-container">
         <ul>
           <li>
             <Link to={`/customer/update/${myuser.userId}`}>Update Profile</Link>
           </li>
           <li>
-            <Link to={`/customer/update/address/${myuser.userId}`}>Update Address</Link>
+            <Link to={`/customer/update/address/${myuser.userId}`}>
+              Update Address
+            </Link>
           </li>
-          <li><Link to={`/customer/update/address/${myuser.userId}`}>View Orders</Link></li>
+          <li>
+            <Link to={`/customer/update/address/${myuser.userId}`}>
+              View Orders
+            </Link>
+          </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle"
-              to=''
+            <Link
+              className="nav-link dropdown-toggle"
+              to=""
               id="navbardrop"
-              data-toggle="dropdown">Prescription</Link>
+              data-toggle="dropdown"
+            >
+              Prescription
+            </Link>
             <div className="dropdown-menu">
-              <Link to={`/prescriptions/${myuser.userId}`} className="dropdown-item">View Prescriptions</Link>
-              <Link to="/" className="dropdown-item">Add Prescription</Link>
-              
-              
+              <Link
+                to={`/prescriptions/${myuser.userId}`}
+                className="dropdown-item"
+              >
+                View Prescriptions
+              </Link>
+              <Link to="/" className="dropdown-item">
+                Add Prescription
+              </Link>
             </div>
           </li>
         </ul>
       </div>
 
-      <div className="logout">
-        <button className="btn btn-primary" onClick={doLogout}>Log Out</button>
+      <div className="logout-container">
+        <button className="logout-button" onClick={doLogout}>
+          Log Out
+        </button>
       </div>
     </div>
   );
 }
 
 export default Sidebar;
-
-
-
-
-
